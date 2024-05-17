081db3af8c2109ebe45746c900d866a349f14d8b
guk30000 <guk30000@mail.ru>
2024-05-17 22:40:43 +0500

    Hw done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Задача 1
+        System.out.println("Задача 1");
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+
+        System.out.println("ФИО сотрудника — " + fullName);
+
+        //Задача 2
+        System.out.println("Задача 2");
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullName.toUpperCase());
+
+        //Задача 3
+        fullName = "Иванов Семён Семёнович";
+        String fullNameCorrect = fullName.replace("ё", "е");
+        System.out.println("Данные ФИО сотрудника — " + fullNameCorrect);
     }
 }
